f0753f0bd6ec33543a7e86e9c02d5a43930964aa
memorysaver <mfa1484@gmail.com>
2015-09-19 19:44:34 +0800

    疑似無法解決debounce的問題 還需要改進電路跟arduino端的程式  先放著先解決其他部分

--- Arduino/toiletDevice/toiletDevice.ino
+++ Arduino/toiletDevice/toiletDevice.ino
@@ -2,17 +2,27 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BT(10, 9);
 const byte ledPin = 13;
+const byte doorStatusPin = 2;
 char val;
-bool doorStatus = true;
+
+static int prevDoorStatus = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(doorStatusPin, INPUT);
   // put your setup code here, to run once:
   BT.begin(9600);
-  BT.println("OK");
 }
 
 void loop() {
+  
+  int doorStatus = 0;
+  
+  //告知這一輪是否已經report過了
+  bool isReported = false;
+  
+  doorStatus = digitalRead(doorStatusPin);
+  
   // put your main code here, to run repeatedly:
   if (BT.available()){
     val = BT.read();
@@ -24,14 +34,27 @@ void loop() {
         digitalWrite(ledPin, 1);
         break;  
       case 'T':
-        //傳送訊息回Server
-        if (doorStatus == 1) {
-           int bytesSent = BT.write("1");
-        }else {
-          int bytesSent = BT.write("0");
-        }
-        
+        reportToServer(doorStatus);
+        isReported = true;
         break;      
     }
+  }
+  
+  if (isReported == false) {
+    if (prevDoorStatus != doorStatus) {
+       reportToServer(doorStatus);
+    }
+  }
+  
+  prevDoorStatus = doorStatus;
+   
+}
+
+bool reportToServer(int status) {
+  //傳送訊息回Server
+  if (status == HIGH) {
+    int bytesSent = BT.write("1");
+  }else {
+    int bytesSent = BT.write("0");
   }
 }
